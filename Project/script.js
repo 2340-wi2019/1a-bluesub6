caa530ac0616b13d9dfbb875514f5c47155b175a
bluesub6 <58628866+bluesub6@users.noreply.github.com>
2019-12-08 20:25:36 -0600

    Assignment01

---
+++ Project/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+
+$('#first-name').text('Jacobi');
+$('#last-name').text('Williams');
+$('#book').append('<li id="Space">Space</li>', '<li id="fish">Aquariums</li>');
+$('.mcc').css({"font-style":"italic","color":"blue"});
+});
